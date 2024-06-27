e18e1f18552e758516a5dce7540eb3f262b973ee
RadiantDerg <jayvier13@gmail.com>
2024-06-27 00:15:28 -0500

    i forgor

--- Types.h
+++ Types.h
@@ -4,13 +4,15 @@
 struct Vector2
 {
 	Vector2(Hedgehog::Math::CVector2 in) : x(in.x()), y(in.y()) {}
+	Vector2(float x, float y) : x(x), y(y) {}
 	float x;
 	float y;
 };
 
 struct Vector3
 {
-	Vector3 (Hedgehog::Math::CVector in) : x(in.x()), y(in.y()), z(in.z()) {}
+	Vector3(Hedgehog::Math::CVector in) : x(in.x()), y(in.y()), z(in.z()) {}
+	Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 	float x;
 	float y;
 	float z;
