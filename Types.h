fa67afd2294f4d820f7af2cb37bac29cb17d3726
RadiantDerg <9061202+RadiantDerg@users.noreply.github.com>
2026-08-09 20:20:41 -0500

    Update Types.h

--- Types.h
+++ Types.h
@@ -3,31 +3,44 @@
 
 struct Vector2
 {
+	float x;
+	float y;
+
+	constexpr Vector2() : x(0.0f), y(0.0f) {}
 	Vector2(float x, float y) : x(x), y(y) {}
 #ifdef BB_OFFSETOF
 	Vector2(Hedgehog::Math::CVector2  in) : x(in.x()), y(in.y()) {}
 	Vector2(Hedgehog::Math::CVector2* in) : x(in->x()), y(in->y()) {}
 #endif
-	float x;
-	float y;
+#ifdef IMGUI_API
+	Vector2(const ImVec2& in) : x(in.x), y(in.y) {}
+#endif
 };
 
 struct Vector3
 {
+	float x;
+	float y;
+	float z;
+
+	constexpr Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
 	Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 #ifdef BB_OFFSETOF
 	Vector3(Hedgehog::Math::CVector  in) : x(in.x()), y(in.y()), z(in.z()) {}
 	Vector3(Hedgehog::Math::CVector* in) : x(in->x()), y(in->y()), z(in->z()) {}
 #endif
-	float x;
-	float y;
-	float z;
-
 	Vector3 operator+=(const Vector3& in)
-	{
+    {
 		this->x += in.x;
 		this->y += in.y;
 		this->y += in.y;
+		return *this;
+	}
+	Vector3 operator-=(const Vector3& in)
+	{
+		this->x -= in.x;
+		this->y -= in.y;
+		this->y -= in.y;
 		return *this;
 	}
 	Vector3 operator*(const Vector3& in)
@@ -37,24 +50,41 @@ struct Vector3
 		this->z * in.z;
 		return *this;
 	}
+	bool operator==(const Vector3& in) const { return (this->x == in.x && this->y == in.y && this->z == in.z); }
 };
 
 struct Vector4
 {
+	float x;
+	float y;
+	float z;
+	float w;
+
 	Vector4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
 #ifdef BB_OFFSETOF
 	Vector4(Hedgehog::Math::CVector4& in) : x(in.x()), y(in.y()), z(in.z()), w(in.w()) {}
 	Vector4(Hedgehog::Math::CVector4* in) : x(in->x()), y(in->y()), z(in->z()), w(in->w()) {}
 #endif
-	float x;
-	float y;
-	float z;
-	float w;
+};
+
+struct Quaternion : public Vector4
+{
+	Quaternion() : Vector4(0.0f, 0.0f, 0.0f, 1.0f) {}
+	Quaternion(float x, float y, float z, float w) : Vector4(x, y, z, w) {}
+	Quaternion(const Vector4& in) : Vector4(in.x, in.y, in.z, in.w) {}
+	Quaternion(const Quaternion& in) : Vector4(in.x, in.y, in.z, in.w) {}
+#ifdef BB_OFFSETOF
+	Quaternion(Hedgehog::Math::CQuaternion& in) : Vector4(in.x(), in.y(), in.z(), in.w()) {}
+	Quaternion(Hedgehog::Math::CQuaternion* in) : Vector4(in->x(), in->y(), in->z(), in->w()) {}
+#endif
 };
 
 struct Matrix44
 {
-	Matrix44(Vector4 a, Vector4 b, Vector4 c, Vector4 d) : r0(a), r1(b), r2(c), r3(d) {}
+	Vector4 r0, r1, r2, r3;
+
+	Matrix44() : r0(1.0f, 0.0f, 0.0f, 0.0f), r1(0.0f, 1.0f, 0.0f, 0.0f), r2(0.0f, 0.0f, 1.0f, 0.0f), r3(0.0f, 0.0f, 0.0f, 1.0f) {}
+	Matrix44(const Vector4& a, const Vector4& b, const Vector4& c, const Vector4& d) : r0(a), r1(b), r2(c), r3(d) {}
 #ifdef BB_OFFSETOF
 	Matrix44(Hedgehog::Math::CMatrix44& in) :
 		r0({ in(0, 0), in(0, 1), in(0, 2), in(0, 3) }),
@@ -62,10 +92,6 @@ struct Matrix44
 		r2({ in(2, 0), in(2, 1), in(2, 2), in(2, 3) }),
 		r3({ in(3, 0), in(3, 1), in(3, 2), in(3, 3) }) {}
 #endif
-	Vector4 r0;
-	Vector4 r1;
-	Vector4 r2;
-	Vector4 r3;
 };
 
 #endif __TYPES_H
